4982b5a1c014203b9415986b8d321a0f4c3c795b
28roederer <mayor.j.bunny@gmail.com>
2025-04-03 02:16:37 -0400

    This is my maze runner game. There are no special instructions but my special item is that there is one block worth 10 points.

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
+# Maze Runner Game
+__version__ = "04.03.2025"
+__author__ = "Judson Roederer"
+
+
+
 import pygame
 import random
+
+#Here are my numorous flint sessions, the first ones I used flint too much and so I had to delete my code and come up with it more on my own. https://app.flintk12.com/activity/pygame-debug-le-1fe068/session/69674867-94ca-4ab1-a891-e8922e81006d      https://app.flintk12.com/activity/pygame-debug-le-1fe068/session/009c6bf4-64e9-4159-8162-081e06c3ccf0         https://app.flintk12.com/activity/pygame-debug-le-1fe068/session/37a5ac99-b8e1-447f-8188-b8419e674a9f          https://app.flintk12.com/activity/pygame-debug-le-1fe068/session/175119e4-919d-4ec3-9009-0d20e304efec     https://app.flintk12.com/activity/pygame-debug-le-1fe068/session/fcd53aad-ce79-443d-80ac-e1ff3d966de9
+#Soloman helped me in class with this project, and so did Gabriel
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -7,20 +16,42 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 PURPLE = (255, 0, 255)
-ghosts = []
 
-# Score settings
-    score = 0
-    high_score = 0
-    game_over = False
-    show_high_score_animation = False
-    high_score_animation_timer = 0
-    HIGH_SCORE_ANIMATION_DURATION = 180
-    font = pygame.font.Font(None, 36)
-    big_font = pygame.font.Font(None, 72)
+
+pygame.font.init()
+score = 0
+high_score = 0
+game_over = False
+show_high_score_animation = False
+high_score_animation_timer = 0
+HIGH_SCORE_ANIMATION_DURATION = 180
+font = pygame.font.Font(None, 36)
+big_font = pygame.font.Font(None, 72)
 
 ghost = pygame.image.load('images/ghost.png')
+
+# Ghost class
+#Where I have the code for the ghost
+
+class Ghost(pygame.sprite.Sprite):
+    def __init__(self, start_x, start_y, ghost_value=1):
+        super().__init__()
+        self.image = pygame.image.load('images/ghost.png')
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.value = ghost_value
+        self.rect = self.image.get_rect()
+        self.rect.y = start_y
+        self.rect.x = start_x
+        self.speed_x = 0
+        self.speed_y = 0
+
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+
 class Wall(pygame.sprite.Sprite):
+#This class repersents a wall that kills the player
     """This class represents the bar at the bottom that the player controls """
 
     def __init__(self, x, y, width, height, color):
@@ -42,7 +73,7 @@ class Wall(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     """ This class represents the bar at the bottom that the
     player controls """
-
+# THIS CLASS REPERSENTS THE PLAYER AND ITS HOW YOU CONTROL IT
     # Set speed vector
     change_x = 0
     change_y = 0
@@ -72,33 +103,18 @@ class Player(pygame.sprite.Sprite):
 
         # Move left/right
         self.rect.x += self.change_x
+        self.rect.y += self.change_y
 
         # Did this update cause us to hit a wall?
         block_hit_list = pygame.sprite.spritecollide(self, walls, False)
-        for block in block_hit_list:
-            # If we are moving right, set our right side to the left side of
-            # the item we hit
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            else:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.left = block.rect.right
-
-        # Move up/down
-        self.rect.y += self.change_y
-
-        # Check and see if we hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
-        for block in block_hit_list:
-
-            # Reset our position based on the top/bottom of the object.
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            else:
-                self.rect.top = block.rect.bottom
+        if block_hit_list:
+            self.rect.y += self.change_y
+            return "JUST_HIT_THE_WALL"
 
 
 class Room(object):
+    # BASE CLASS FOR THE ROOMS
+    # ROOM 1,2,3 ARE SUB-CLASSES THAT SHOW WHAT EACH ROOM DOES AND IS THE CODE FOR THAT ROOM
     """ Base class for all rooms. """
 
     # Each room has a list of walls, and of enemy sprites.
@@ -181,42 +197,19 @@ class Room3(Room):
         for x in range(150, 700, 100):
             wall = Wall(x, 200, 20, 200, WHITE)
             self.wall_list.add(wall)
-class Enemies(Room):
-
-    if ghost_spawn_timer >= current_spawn_interval * 1.5:  # ghost spawn slower
-        ghost_spawn_timer = 0
-        new_ghost_rect = ghost_image.get_rect()
-
-        if random.random() < SIDE_GHOST_SPAWN_CHANCE:
-            ghost.x = -new_ghost_rect.width
-            new_ghost_rect.y = random.randint(0, WINDOW_HEIGHT // 2)
-            new_ghost = Ghost(new_ghost_rect, SIDE_GHOST_SPEED_X, SIDE_GHOST_SPEED_Y)
-        else:
-            new_ghost_rect.x = random.randint(0, WINDOW_WIDTH - new_ghost_rect.width)
-            new_ghost_rect.y = -new_ghost_rect.height
-            new_ghost = ghost(new_ghost_rect, 0, GHOST_SPEED)
-
-        ghosts.append(new_ghost)
-
-
-    for ghost in ghosts[:]:
-        ghost.rect.x += ghost.speed_x
-        ghost.rect.y += ghost.speed_y
-        if (ghost.rect.colliderect(soldier_rect) or
-                ghost.rect.top > WINDOW_HEIGHT or
-                ghost.rect.left > WINDOW_WIDTH):
-            game_over = True
-            if score > high_score:
-                high_score = score
-                show_high_score_animation = True
-                high_score_animation_timer = HIGH_SCORE_ANIMATION_DURATION
-            break
-
-
 
 
 def main():
+    #MAIN FUNCTION THIS IS WHERE ALL OF THE OTHER CODE HAPPENS
     """ Main Program """
+    # Define ghost positions
+    global high_score
+    ghost_positions = [(100, 100), (200, 200), (323, 475), (575, 125),
+                       (678, 324), (365, 765), (564, 342), (123, 528)]
+
+    lives = 1
+    score = 0
+    ghost_group = pygame.sprite.Group()
 
     # Call this function so the Pygame library can initialize itself
     pygame.init()
@@ -248,12 +241,21 @@ def main():
 
     clock = pygame.time.Clock()
 
-    done = False
+    # Create ghosts with one special ghost
+    for pos in ghost_positions:
+        if pos == (123, 528):
+            new_ghost = Ghost(pos[0], pos[1], ghost_value=10)
+        else:
+            new_ghost = Ghost(pos[0], pos[1])
+        ghost_group.add(new_ghost)
 
+
+    done = False
     while not done:
 
-        # --- Event Processing ---
+        screen.fill(BLACK)
 
+        # Process events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -279,9 +281,40 @@ def main():
                     player.changespeed(0, -5)
 
         # --- Game Logic ---
+        # CHECKS THE WALLS FOR COLLISIONS
+        if pygame.sprite.spritecollide(player, current_room.wall_list, False):
+            lives -= 1
 
+            if lives == 0:
+                screen.fill(BLACK)
+                sound_effect = pygame.mixer.Sound('sounds/blip3.wav')
+                sound_effect.play()
+                gameover_text = font.render("Game Over", True, (255, 255, 255))
+                screen.blit(gameover_text, (200, 200))
+                if score > high_score:
+                    high_score = score
+                high_score_text = font.render(f'High Score: {high_score}', True, WHITE)
+                screen.blit(high_score_text, (550, 80))
+
+                pygame.display.flip()  # Make sure to update the display
+                pygame.time.wait(2000)  # Wait 2 seconds to show game over
+
+
+                pygame.quit()
+                done = True
+
+        # Move the player
         player.move(current_room.wall_list)
 
+        # Update ghosts
+        ghost_group.update()
+
+        # Check for ghost collisions
+        ghosts_collected = pygame.sprite.spritecollide(player, ghost_group, True)
+        for collected_ghost in ghosts_collected:
+            score += collected_ghost.value
+
+        # TRANSITIONS YOU FROM ROOM TO ROOM
         if player.rect.x < -15:
             if current_room_no == 0:
                 current_room_no = 2
@@ -310,18 +343,24 @@ def main():
                 current_room = rooms[current_room_no]
                 player.rect.x = 0
 
-        # --- Drawing ---
-        screen.fill(BLACK)
-
+        # Draws everything
         movingsprites.draw(screen)
         current_room.wall_list.draw(screen)
+        ghost_group.draw(screen)
+
+
+        score_text = font.render(f'Score: {score}', True, WHITE)
+        screen.blit(score_text, (650, 10))
+
+        lives_text = font.render(f'Lives: {lives}', True, WHITE)
+        screen.blit(lives_text, (650, 50))
 
         pygame.display.flip()
 
         clock.tick(60)
-
     pygame.quit()
 
 
 if __name__ == "__main__":
     main()
+
